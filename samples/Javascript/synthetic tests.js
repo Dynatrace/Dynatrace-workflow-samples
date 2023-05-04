bf68b439db090456e5c8fae9559a2d40318ea322
amohrd <96291004+amohrd@users.noreply.github.com>
2023-05-04 16:14:01 +0200

    Create synthetic tests.js

---
+++ samples/Javascript/synthetic tests.js
@@ -0,0 +1,34 @@
+// copy this snippet into the first JavaScript action
+import { syntheticOnDemandMonitorExecutionsClient } from '@dynatrace-sdk/client-classic-environment-v2';
+
+
+// First action.
+export default async function ({ execution_id }) {
+
+    // here is your synthetic monitor Id to execute
+    const monitorIdParam = "HTTP_CHECK-2E0B59F268D6E943";
+
+    const config = {
+        body: {
+            monitors: [{monitorId: monitorIdParam }], 
+        }
+    }
+
+    const executionResponse = await syntheticOnDemandMonitorExecutionsClient.execute(config);
+    const executionId = executionResponse.triggered[0]?.executions[0]?.executionId;
+    return executionId;
+}
+
+// Copy this snippet into the first JavaScript action, so that it loops until it succeeded.
+export default async function ({ execution_id }) {
+
+    let executionResult
+    do {
+        executionResult = await syntheticOnDemandMonitorExecutionsClient.getExecution(
+            { executionId: executionId }
+        );
+    } while (executionResult.executionStage !== 'EXECUTED');
+
+    console.log("Execution result : " + JSON.stringify(executionResult));
+    return { executionResult };
+}
