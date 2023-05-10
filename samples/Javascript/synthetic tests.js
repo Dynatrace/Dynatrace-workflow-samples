ed87e1645daa9bb032243ef6549b4d2a472dd369
amohrd <96291004+amohrd@users.noreply.github.com>
2023-05-10 16:53:47 +0200

    Add comments, include reference to previous tasks

--- samples/Javascript/synthetic tests.js
+++ samples/Javascript/synthetic tests.js
@@ -1,4 +1,4 @@
-// copy this snippet into the first JavaScript action
+// copy this snippet into the first JavaScript action, name the task e.g. "trigger_synthetic"
 import { syntheticOnDemandMonitorExecutionsClient } from '@dynatrace-sdk/client-classic-environment-v2';
 
 
@@ -19,16 +19,25 @@ export default async function ({ execution_id }) {
     return executionId;
 }
 
-// Copy this snippet into a second JavaScript action and set it to retry every e.g. 30 seconds.
+// Copy this snippet into a second JavaScript action 
+// In the options tab activate Retry on error. The number of retries and delay should exceed your average syntehtic execution time, e.g. 4 retries with 30 seconds
+// Adpapt the timeout to reflect the maximum execution time, e.g. 130 seconds
+import { syntheticOnDemandMonitorExecutionsClient } from '@dynatrace-sdk/client-classic-environment-v2';
+import { executionsClient } from '@dynatrace-sdk/client-automation';
+
+
 export default async function ({ execution_id }) {
 
-    let executionResult
-    do {
-        executionResult = await syntheticOnDemandMonitorExecutionsClient.getExecution(
-            { executionId: executionId }
-        );
-    } while (executionResult.executionStage !== 'EXECUTED');
+  const getItemsResult = await executionsClient.getTaskExecutionResult({ executionId: execution_id, id: "trigger_synthetic" });
+  let executionId = getItemsResult.executionId
+    
+  let executionResult
+  do {
+    executionResult = await syntheticOnDemandMonitorExecutionsClient.getExecution(
+      { executionId: executionId }
+    );
+  } while (executionResult.executionStage !== 'EXECUTED');
 
-    console.log("Execution result : " + JSON.stringify(executionResult));
-    return { executionResult };
+  console.log("Execution result : " + JSON.stringify(executionResult));
+  return { executionResult };
 }
